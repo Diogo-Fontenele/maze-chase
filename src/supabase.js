fd117db45e1c3527b138e447a7fdc36132e2180a
Diogo-Fontenele <82485252+Diogo-Fontenele@users.noreply.github.com>
2026-06-11 06:33:31 -0300

    Add files via upload

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://wlglksboakpsibgykymx.supabase.co'
-const supabaseKey = 'sb_publishable_hCJq1fKTJBmvy0AFaR7jyg_dExXymV7'
+const SUPABASE_URL = 'https://wlglksboakpsibgykymx.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_hCJq1fKTJBmvy0AFaR7jyg_dExXymV7'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
